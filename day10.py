6e0f9a47fc3243e4dcdeddef39900613c62361e2
Malcolm <56974882+malcolmcdixon@users.noreply.github.com>
2021-12-10 11:06:24 +0000

    First commit

---
+++ day10.py
@@ -0,0 +1,38 @@
+navigations = []
+with open("day10_input.txt") as f:
+    lines = f.readlines()
+    for line in lines:
+        data = [d for d in line.rstrip()]
+        navigations.append(data)
+
+open_chunk = ["(", "[", "{", "<"]
+close_chunk = [")", "]", "}", ">"]
+
+points = [3, 57, 1197, 25137]
+completion_scores = []
+
+syntax_error_score = 0
+
+for navigation in navigations:
+    chunks = []
+    corrupt = False
+    for char in navigation:
+        if char in open_chunk:
+            chunks.append(char)
+        elif open_chunk.index(chunks[-1]) == close_chunk.index(char):
+            chunks.pop()
+        else:
+            corrupt = True
+            syntax_error_score += points[close_chunk.index(char)]
+            break
+
+    if not corrupt:
+        score = 0
+        for char in reversed(chunks):
+            score = score * 5 + open_chunk.index(char) + 1
+
+        completion_scores.append(score)
+
+print(f"Syntax Score: {syntax_error_score}")
+completion_scores.sort()
+print(f"Completion Score: {completion_scores[int((len(completion_scores)-1)/2)]}")
